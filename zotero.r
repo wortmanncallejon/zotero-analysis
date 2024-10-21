b7db7b10171aa9d4299c3e007b714bd99bff8fdc
FWC <felix@wortmanncalejon.de>
2024-10-21 19:31:56 +0200

    Initial commit

---
+++ zotero.r
@@ -0,0 +1,15 @@
+pacman::p_load(dplyr, ggplot2, readr, lubridate)
+
+read_csv("bibliothek.csv", show_col_types = FALSE) |>
+    mutate(`Date Added` = as.Date(`Date Added`)) |>
+    arrange(`Date Added`) |>
+    summarise(n = n(),
+              .by = `Date Added`) |>
+    mutate(stock = cumsum(n)) |>
+    ggplot(aes(`Date Added`, stock)) +
+    scale_y_continuous("Titles in Zotero library", labels = scales::label_comma(1)) +
+    scale_x_date("Date", date_breaks = "1 year", date_labels = "%Y") +
+    geom_line() +
+    theme_light()
+
+ggsave("zotero.png", width = 16, height = 9, units = "cm")
