3b45973784f21f83798fcedfc2b0d5cca87285f5
FWC <felix@wortmanncallejon.de>
2025-01-14 14:12:56 +0100

    🔄 Update Script

--- zotero.r
+++ zotero.r
@@ -6,9 +6,11 @@ read_csv("library.csv", show_col_types = FALSE) |>
     summarise(n = n(),
               .by = `Date Added`) |>
     mutate(stock = cumsum(n)) |>
+    #filter(`Date Added` > as.Date("2024-09-01")) |>
     ggplot(aes(`Date Added`, stock)) +
     scale_y_continuous("Titles in Zotero library", labels = scales::label_comma(1)) +
-    scale_x_date("Date", date_breaks = "1 year", date_labels = "%Y") +
+    #scale_x_date("Date", date_breaks = "1 month", date_labels = "%d. %b %Y") +
+    scale_x_date("Date", date_breaks = "1 year", date_labels = "%d. %b %Y") +
     geom_line() +
     theme_light()
 
